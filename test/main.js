851e5c2b6b9f61e3d6db8aaa72a2000c531b377e
mobenar <tomas.enarsson@mobenga.com>
2015-01-13 14:50:11 +0100

    test case added

--- test/main.js
+++ test/main.js
@@ -1,6 +1,19 @@
 var fs = require('fs');
 var expect = require('chai').expect;
 
+var GML = require('../index');
+
 describe('GML', function () {
+
+	it.skip('should parse', function () {
+
+		var gml = GML.parse(fs.readFileSync('test/graph.gml', 'utf8'));
+
+		var nodeIds = gml.nodes.map(function (node) { return node.id; });
+		var edgeLabels = gml.edges.map(function (edge) { return edge.label; });
+
+		expect(nodeIds).to.have.members([1, 2, 3, 4, 5, 6, 7]);
+		expect(edgeLabels).to.have.members(['A->B', 'A->C', 'B->D', 'B->E', 'C->F', 'C->G']);
+	});
 
 });
